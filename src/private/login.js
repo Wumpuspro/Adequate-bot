847661fc393a6e017c3822d1ff1031917f4531f1
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-14 21:31:25 +0545

    don't edit it

---
+++ src/private/login.js
@@ -0,0 +1,6 @@
+export const password = process.env.token;
+export const idServer = process.env.IDSERVER;
+export const dbName = process.env.DBNAME;
+
+export const webhookLogs = process.env.WEBHOOKLOGS;
+export const webhookWelcome = process.env.WEBHOOKWELCOME;
