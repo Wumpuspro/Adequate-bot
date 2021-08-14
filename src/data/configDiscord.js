186dc93841d40ee2e20d2f0664f077fb500767d1
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-14 22:01:19 +0545

    configuration

---
+++ src/data/configDiscord.js
@@ -0,0 +1,12 @@
+export const prefix = 'a!';
+export const splitStrings = [true, ';']; // true 
+export const language = 'en';
+export const color = '#303136';
+
+export const devs = [['Npg#0001', '852219497763045398']];
+
+export const suggestionChannel = ['875005195195670528', '875005195195670528']; // [0]:idServer [1]:idChannel
+export const reportsChannel = ['875005195195670528', '875005195195670528']; // [0]:idServer [1]:idChannel
+export const discordV = 'v13.0.0';
+export const nodeV = 'v16.6.1';
+export const botV = 'v1.0.0';
